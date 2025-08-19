49fa2b4a64c74c44c237192fe13fbe44b1950519
janguzin <mongsil1112@naver.com>
2025-08-19 16:38:38 +0900

    feat: column 실습 추가

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -1,41 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:smwu_202508/screen/container/container_screen.dart';
 import 'package:smwu_202508/screen/container/container_practice_screen.dart';
+import 'package:smwu_202508/screen/column/column_screen.dart';
+import 'package:smwu_202508/screen/column/column_practice_screen.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
+
+  void _go(BuildContext context, Widget page) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => page),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ContainerScreen(),
-                  ),
-                );
-              },
-              child: const Text("Container"),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ContainerPracticeScreen(),
-                  ),
-                );
-              },
-              child: const Text("container 실습"),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: () => _go(context, const ContainerScreen()),
+                child: const Text('Container'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () => _go(context, const ContainerPracticeScreen()),
+                child: const Text('Container 실습'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () => _go(context, const ColumnScreen()),
+                child: const Text('Column'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () => _go(context, const ColumnPracticeScreen()),
+                child: const Text('Column 실습'),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/screen/column/column_practice_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ColumnScreen extends StatelessWidget {
+  const ColumnScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('column'),),
+
+      body: Container(
+        width: 300,
+        color: Colors.purple,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 100, height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100, height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100, height: 100,
+              color: Colors.green,
+            ),
+          ],
+
+        ),
+      ),
+    );
+  }
+}
