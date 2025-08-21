329e80b5389c78fe96358baf5838ca6eb1f1fb00
janguzin <mongsil1112@naver.com>
2025-08-21 14:59:47 +0900

    feat: 함수 배움

---
+++ test/variable.dart
@@ -0,0 +1,35 @@
+class Variable {
+
+
+
+  Variable(){
+    String name = '장재경';
+    name = 'jaekyung';
+    int age = 37;
+    bool check = true;
+    bool check2 = false;
+    double weight = 65;
+
+    var animal = 'black bunny';
+    animal = 'black pup';
+
+    dynamic home = false;
+    home = true;
+    home = '안현시';
+
+    String? smwu = 'yes';
+    smwu = null;
+    int? age2 = 1882;
+    age2 = null;
+    bool? h2 = false;
+    h2 = null;
+
+    final String name3 = '창수재경아뭐하니';
+    const String address = 'aaa';
+
+
+  }
+
+
+
+}
